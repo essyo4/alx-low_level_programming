67b7b7a1445b764cb12d914fb71c4d0d1f30e3e5
essyo4 <estherkathmbi46@gmail.com>
2022-10-19 08:54:01 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,6 +2,7 @@
 
 /**
  * times_table - prints the 9 times table
+ *
  */
 void times_table(void)
 {
@@ -19,10 +20,14 @@ void times_table(void)
 			prod = num * mult;
 
 			if (prod <= 9)
+			{
 				_putchar(' ');
+			}
 			else
+			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
