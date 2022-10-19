215f45d475e87f1aee275b103f95a7925e4ce096
essyo4 <estherkathmbi46@gmail.com>
2022-10-19 14:59:08 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,11 +15,13 @@ void times_table(void)
 			k = j * i;
 			if (j == 0)
 			{
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			else if (k >= 10)
