79695423109a8d51e9b888c6eb92feabe4ff28d1
essyo4 <estherkathmbi46@gmail.com>
2022-10-21 06:38:40 -0700

    prints line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,12 @@ void print_line(int n)
 {
 	int l;
 
-	if (n > 0)
-	{
-		for (l > 0; l < n; l++)
-			_putchar('_');
-	}
+	if (n <= 0)
+		_putchar('\n');
 	else
 	{
+		for (l = 0; l < n; l++)
+			_putchar(95);
 		_putchar('\n');
 	}
 }
