6ca02bebce31513cad4957a721daa7657ffbc202
essyo4 <estherkathmbi46@gmail.com>
2022-10-21 06:03:01 -0700

    prints straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line
+ * @n: number of time character is printed
+ */
+
+void print_line(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l > 0; l < n; l++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
