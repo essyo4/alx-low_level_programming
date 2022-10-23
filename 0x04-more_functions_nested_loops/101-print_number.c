4c983bdf99784d75e9923509c6327cd00fdb7744
essyo4 <estherkathmbi46@gmail.com>
2022-10-23 02:43:06 -0700

    prints number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer
+ * @n: number to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int d = n;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		d = -d;
+	}
+	if ((d / 10) > 0)
+	{
+		print_number(d / 10);
+	}
+	_putchar((d % 100 + '0'));
+}
