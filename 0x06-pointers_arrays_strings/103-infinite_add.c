3f50e9b7a81e7a71dc3841782ed0a518985b5572
essyo4 <estherkathmbi46@gmail.com>
2022-10-29 12:33:01 -0700

    task 10

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+/**
+ * add_strings - adds numbers stored in strings.
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to store results
+ * @r_index: current index of buffer
+ * Return:pointer to result if r stores sum else 0
+ */
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + 'o';
+		tens = num / 10;
+	}
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
